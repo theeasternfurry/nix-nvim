bb4c1d103c3867f0cb841a6c3978d9d9da6e01dd
theeasternfurry <theeasternfurry@outlook.com>
2026-01-11 14:01:00 +0700

    feat: use space as leader and add yazi file manager

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,3 +1,9 @@
-require('which-key').setup {
+local wk = require("which-key")
+
+wk.setup({
   preset = 'helix'
-}
+})
+
+wk.add({
+  { "<leader>e", "<cmd>Yazi<cr>", desc = "Yazi File explorer" },
+})
