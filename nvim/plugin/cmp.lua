c05b547c149d0d5859692659ec936b66bb6cb91d
theeasternfurry <theeasternfurry@outlook.com>
2026-01-12 23:16:09 +0700

    feat: add cmp config for select and choose completion

---
+++ nvim/plugin/cmp.lua
@@ -0,0 +1,15 @@
+local cmp = require("cmp")
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = "codecompanion" },
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
+})
